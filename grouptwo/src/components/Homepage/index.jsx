f47274aba2b385a912695e0ed9ba84fc5890d133
Patrick Mutua <pawatechsys@gmail.com>
2026-01-29 21:50:05 +0300

    add homepage

--- grouptwo/src/components/Homepage/index.jsx
+++ grouptwo/src/components/Homepage/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import Navbar from "./components/NavBar/Navbar";
+import Herosection from "./components/HeroSection/Herosection";
+import Services from "./components/OurServices/Services";
+import About from "./components/AboutUs/About";
+import Footer from "./components/Footer/Footer.jsx";
+
 function Homepage() {
   return (
-    <div>
-      <h1>Home page Coming Soon</h1>
+    <div className="bg-gray-50 w-full">
+      <Navbar />
+      <Herosection />
+      <Services />
+      <About />
+      <Footer />
     </div>
   );
 }
+
 export default Homepage;
