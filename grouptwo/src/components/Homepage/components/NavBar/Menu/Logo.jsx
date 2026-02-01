6c6fa911eb0cc2c9146bcdfb13baac15005d3a09
Patrick Mutua <pawatechsys@gmail.com>
2026-02-01 13:22:38 +0300

    add service details

--- grouptwo/src/components/Homepage/components/NavBar/Menu/Logo.jsx
+++ grouptwo/src/components/Homepage/components/NavBar/Menu/Logo.jsx
@@ -1,7 +1,13 @@
 function Logo() {
   return (
-    <div>
-      <h1 className="font-bold text-gray-800 ml-20 text-3xl">NexusFLow</h1>
+    <div className="flex gap-5 ml-20">
+      {" "}
+      <div className="w-10 h-10 rounded-full bg-green-500 flex items-center justify-center text-white font-bold">
+        NF
+      </div>
+      <div>
+        <h1 className="font-bold text-gray-800 text-3xl">NexusFLow</h1>
+      </div>
     </div>
   );
 }
--- grouptwo/src/components/Homepage/components/NavBar/Navbar.jsx
+++ grouptwo/src/components/Homepage/components/NavBar/Navbar.jsx
@@ -4,7 +4,7 @@ import Menubtn from "./Menu/Menubtn";
 
 function Navbar() {
   return (
-    <div className=" flex items-center w-screen justify-between p-4 mr-3 ml-3 bg-white shadow-md">
+    <div className=" flex items-center fixed top-0 left-0 z-50 w-full justify-between p-4 mr-3 ml-3 bg-white shadow-md">
       <Logo />
       <Menu />
       <Menubtn />
